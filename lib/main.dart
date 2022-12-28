86ef41c196580fff07311cbd4d68e875e09556da
sunglimlee <steve.patriot@gmail.com>
2022-12-28 12:49:36 -0500

    Cloud Firebase 의 Flutter 연결을 위한 3 단계 완료 - main 함수만 바꾸어주었다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart'; // install FlutterFire_CLI
+import 'package:better_touching_staff/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+/// 여기 파이어 베이스를 연결하기 위한 초기화 작업을 해주어야 한다.
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options : DefaultFirebaseOptions.currentPlatform );
+
   runApp(const MyApp());
 }
 
